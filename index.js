4fcf2bcf0a342d5c1d97cc275e934011c8ebaa31
sathya-narayana <ksathya2030@gmail.com>
2022-07-18 01:31:02 +0530

    image constructer is created for testing to add design of boundries

--- index.js
+++ index.js
@@ -7,15 +7,17 @@ canvas.height = innerHeight
 class Boundary {
     static width = 40
     static height = 40
-    constructor({position}) {
+    constructor({position, image}) {
         this.position = position
         this.width = 40
         this.height = 40
+        this.image = image
     }
     
     draw() {
-        c.fillStyle = 'blue'
-        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+       // c.fillStyle = 'blue'
+       // c.fillRect(this.position.x, this.position.y, this.width, this.height)
+       c.drawImage(this.image, this.position.x, this.position.y)
     }
 }
 
@@ -73,13 +75,16 @@ const keys = {
 let lastKey = ''
 const map = [
     ['-', '-', '-', '-', '-','-', '-'],
-    ['-', ' ', ' ', ' ', ' ',' ', '-'],
-    ['-', ' ', '-', ' ', '-',' ', '-'],
-    ['-', ' ', ' ', ' ', ' ',' ', '-'],
-    ['-', ' ', '-', ' ', '-',' ', '-'],
-    ['-', ' ', ' ', ' ', ' ',' ', '-'],
+    ['|', ' ', ' ', ' ', ' ',' ', '|'],
+    ['|', ' ', '-', ' ', '-',' ', '|'],
+    ['|', ' ', ' ', ' ', ' ',' ', '|'],
+    ['|', ' ', '-', ' ', '-',' ', '|'],
+    ['|', ' ', ' ', ' ', ' ',' ', '|'],
     ['-', '-', '-', '-', '-','-', '-']
 ]
+
+const image = new Image()
+image.src = './img/pipeHorizontal.png'
 
 map.forEach((row, i ) => {
     row.forEach((symbol, j) =>{
@@ -90,7 +95,8 @@ map.forEach((row, i ) => {
                     position: {
                         x: Boundary.width * j,
                         y: Boundary.height * i
-                    }
+                    },
+                    image : image
                 })
             )
             break
